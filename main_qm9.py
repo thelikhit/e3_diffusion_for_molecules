8de6d49ac5baa0a88096b2fce40e3ca5b1245321
vinhsuhi <vinhbachkhoait@gmail.com>
2025-10-14 23:07:57 +0200

    flag

--- main_qm9.py
+++ main_qm9.py
@@ -217,6 +217,7 @@ if args.resume is not None:
 
 best_nll_val = 1e8
 best_nll_test = 1e8
+print("FLAG: JOINT TRAINING")
 for epoch in range(args.start_epoch, args.n_epochs):
     start_epoch = time.time()
     train_epoch(args=args, loader=dataloaders['train'], epoch=epoch, model=model, model_dp=None,
